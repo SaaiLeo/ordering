2b898d6774a88024e302ade33caa2347c3a4ff91
Leo <u6520057@au.edu>
2024-09-03 01:23:04 +0700

    Add button and page control

---
+++ coffee/Extension/UIVIew+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIVIew+Extension.swift
+//  coffee
+//
+//  Created by Sei Mouk on 3/9/24.
+//
+
+import Foundation
---
+++ coffee/Model/onBoardingSlide.swift
@@ -0,0 +1,8 @@
+//
+//  onBoardingSlide.swift
+//  coffee
+//
+//  Created by Sei Mouk on 3/9/24.
+//
+
+import Foundation
