b025c1bf04ba30e6af901c098538e0573058d3ff
Leo <u6520057@au.edu>
2024-09-03 15:27:28 +0700

    .

--- coffee/View/onBoardingSlides/onBoardingSlidesCell.swift
+++ coffee/View/onBoardingSlides/onBoardingSlidesCell.swift
@@ -9,4 +9,15 @@ import UIKit
 
 class onBoardingSlidesCell: UICollectionViewCell {
     
+    static let identifier = String(describing: onBoardingSlidesCell.self)
+    
+    @IBOutlet weak var slideImage: UIImageView!
+    @IBOutlet weak var slideTitleLabel: UILabel!
+    @IBOutlet weak var slideDescriptionLabel: UILabel!
+    
+    func setup (_ slide: onBoardingSlide) {
+        slideImage.image = slide.image
+        slideTitleLabel.text = slide.title
+        slideDescriptionLabel.text = slide.description
+    }
 }
